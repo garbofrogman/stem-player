8be2a12acaa3462295b5a182b61fde3726cac6e2
outlandishflmngo <102494168+garbofrogman@users.noreply.github.com>
2025-07-03 19:32:21 -0400

    delete some clutter

--- dist/waveform-playlist/js/fileloader.js
+++ dist/waveform-playlist/js/fileloader.js
@@ -12,36 +12,22 @@ fs.readdir('../media/audio/Weezer/', (err, files) => {
 var http = require('http');
 
 http.createServer( function(req, res) {
-    if (req.method == 'POST') {
-		var msg = '';
-        req.on('data', function (data) {
-            msg += data;
-        });
-        req.on('end', function () {
-			if(msg === 'button=1') {
-				res.writeHead(200, {'Content-Type': 'text/html'});
-				// res.end(printTracks());	// this is for the web page.
-        // document.getElementById('tracklist').innerHTML = files
-			  res.end(`<html>
-			   <body>` + files + `
-			   </body>
-			   </html>`);
-
-				console.log(msg); // this is to show in the server console.
-			}
-			else {
-				res.writeHead(400, {'Content-Type': 'text/html'});
-				res.end('unexpected message (POST request) received: ' + msg);					
-			}
-        });
-    } else {
+  if (req.method == 'POST') {
+		if(msg === 'button=1') {
 			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.end(`<html>
-			<body>
-				As you requested, you should start with your static HTML file instead of browsing the URL directly.
-			</body>
-			</html>`);
-	}
+			// res.end(printTracks());	// this is for the web page.
+      // document.getElementById('tracklist').innerHTML = files
+		  res.end(`<html>
+		   <body>` + files + `
+		   </body>
+		   </html>`);
+			console.log(msg); // this is to show in the server console.
+		}
+		else {
+			res.writeHead(400, {'Content-Type': 'text/html'});
+			res.end('unexpected message (POST request) received: ' + msg);					
+		}
+  }
 }).listen(8081);
 
 function test() {
